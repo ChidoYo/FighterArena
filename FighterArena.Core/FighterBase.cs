1d0d3dc66c8a790c09a4fddd41e144820f258fe2
LDS\dannystaten <ds@ldschurch.org>
2017-11-15 09:48:30 -0700

    validation, string output, and project structure tweak

---
+++ FighterArena.Core/FighterBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FighterArena.Core
+{
+    public class FighterBase
+    {
+        protected string Name { get; set; }
+        protected int Attack { get; set; }
+        protected int Defense { get; set; }
+        protected int Health { get; set; }
+
+        public FighterBase(string name, int attack, int defense, int health)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+            Health = health;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} \n Health: {Health} \n Attack: {Attack} \n Defense: {Defense}";
+        }
+    }
+}
--- FighterArena/Program.cs
+++ FighterArena/Program.cs
@@ -1,39 +1,36 @@
-﻿using System;
+﻿using FighterArena.Core;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace FighterArena
 {
-    class Fighter
-    {
-        public string Name { get; set; }
-        public int Attack { get; set; }
-        public int Defense { get; set; }
-        public int Health { get; set; }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Fighter Arena!\n");
-
+            var health = 0;
+            var defense = 0;
+            var attack = 0;
+            var name = String.Empty;
             while (true)
             {
-                Fighter UserFighter = new Fighter();
-
+           
                 Console.WriteLine("Enter your fighter's name...");
-                UserFighter.Name = Console.ReadLine();
+                name = Console.ReadLine();
 
                 Console.WriteLine("\nEnter your fighter's Attack level... (1 to 10)");
-                UserFighter.Attack = int.Parse(Console.ReadLine());
+                attack = CollectInt(1,10);
 
                 Console.WriteLine("\nEnter your fighter's Defense level... (1 to 10)");
-                UserFighter.Defense = int.Parse(Console.ReadLine());
+                defense = CollectInt(1, 10);
 
                 Console.WriteLine("\nEnter your fighter's Health level... (1 to 10)");
-                UserFighter.Health = int.Parse(Console.ReadLine());
+                health = CollectInt(1, 10);
 
-                Console.WriteLine("\nYour fighter's name is {0} \nAttack level {1} \nDefense level {2} \nHealth level {3}", 
-                    UserFighter.Name, UserFighter.Attack, UserFighter.Defense, UserFighter.Health);
+                var fighter = new FighterBase(name, attack, defense, health);
+                Console.WriteLine(fighter.ToString());
 
                 Console.WriteLine("\nQuit? Y / Q");
                 string response = Console.ReadLine();
@@ -47,6 +44,26 @@ namespace FighterArena
                     continue;
                 }
             }
+
+
+        }
+
+        private static int CollectInt(int min, int max)
+        {
+            var valid = false;
+            int value = min - 1;
+            while (!valid)
+            {
+                if (!int.TryParse(Console.ReadLine(), out value) || value > max || value < min)
+                {
+                    Console.WriteLine("invalid number, please try again.  Must be between {0} and {1}", min, max);
+                }
+                else
+                {
+                    valid = true;
+                }
+            }
+            return value;
         }
     }
 }
